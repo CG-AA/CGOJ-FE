ecf14c2e238eaeb6c114b0d839bdf20a366825fa
CG-AA <syankuan@gmail.com>
2024-06-21 14:15:30 +0800

    auto verify JWT

--- src/API/api.js
+++ src/API/api.js
@@ -1,9 +1,16 @@
-import {useCallback} from 'react';
+import {useCallback, useContext} from 'react';
 
 import settings from '../settings.json';
+import { NotificationContext } from '../components/notifications/NotificationList';
+import obtainJWT from '../functions/obtainJWT';
 
+/**
+ * Custom hook for making API calls.
+ * @returns {Object} An object containing the `callAPI` function.
+ */
 const useAPI = () => {
     const callAPI = useCallback((route, method, data) => {
+        const { addNotification } = useContext(NotificationContext);
         return new Promise(async (resolve, reject) => {
             try {
                 const options = {
@@ -21,8 +28,17 @@ const useAPI = () => {
                 const response = await fetch(settings.CGBE_origin+route, options);
     
                 if (!response.ok) {
-                    const errorResponse = await response.clone().json();
-                    reject(errorResponse.error);
+                    const error = await response.text();
+                    if (error.startsWith("Failed to verify JWT:")){
+                        if(localStorage.getItem('jwt')){
+                            localStorage.removeItem('jwt');
+                            addNotification(2, 'Session expired, please log in again');
+                        }else {
+                            await obtainJWT();
+                        }
+
+                    }
+                    reject(error);
                 }
     
                 const responseData = await response.json();
