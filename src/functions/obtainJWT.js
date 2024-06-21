0a0e3457635c09a41f4b10757c26491c9742db38
CG-AA <syankuan@gmail.com>
2024-06-21 14:15:04 +0800

    let obtainJWT can hendel login

--- src/functions/obtainJWT.js
+++ src/functions/obtainJWT.js
@@ -1,21 +1,17 @@
-import React, { useState } from "react";
-
+import { useContext } from "react";
 import useAPI from "../API/api";
 import { NotificationContext } from "../components/notifications/NotificationList";
 
-const obtainJWT = async () => {
-    const addNotification = useState(NotificationContext);
-    const { _return, loading, error, callAPI } = useAPI();
-    let jwt = localStorage.getItem('jwt');
-    if (!jwt) {
-        await callAPI('/obtain-jwt', {});
-        if(error) {
-            addNotification(2, error);
-            return;
-        }
-        localStorage.setItem('jwt', _return);
+const obtainJWT = async (email, password) => {
+    const { addNotification } = useContext(NotificationContext);
+    const { callAPI } = useAPI();
+    const response = await callAPI('/login', 'POST', { email, password });
+    if (response.status === 200) {
+        localStorage.setItem('jwt', response.jwt);
+        addNotification(0, 'Successfully logged in as ' + (email || 'guest'));
+    } else {
+        addNotification(1, 'Failed to login');
     }
 }
-
 
 export default obtainJWT;
