49ef12ee3c904802e6dee8244a36931680750adb
CG-AA <syankuan@gmail.com>
2024-08-03 01:10:15 +0800

    add some css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Notification from './notification/Notification';
+import Header from './pages/header/Header';
 
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
       </Routes>
       <Notification />
     </Router>
