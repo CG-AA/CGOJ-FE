77b6f4ae09df81d2b2c81cd5c2a28e3d269d1aa5
CG-AA <syankuan@gmail.com>
2024-08-02 17:29:35 +0800

    notification using toastify

---
+++ src/notification/Notification.js
@@ -0,0 +1,14 @@
+// Notification.js
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Notification = () => {
+    return <ToastContainer />;
+};
+
+export const notify = (message) => {
+    toast(message);
+};
+
+export default Notification;
