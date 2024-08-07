c9cbe4f368b0a618f23614bbfc806a9ddd20db09
CG-AA <syankuan@gmail.com>
2024-08-07 21:09:54 +0800

    header

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 function Header() {
     const [authButton, setAuthButton] = useState('');
+    const [userName, setUserName] = useState('');
 
     useEffect(() => {
         const storedUserName = localStorage.getItem('userName');
