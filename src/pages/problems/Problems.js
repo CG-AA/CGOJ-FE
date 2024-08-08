e2c08ec156763d9117300bf76e7d869d623f8db8
CG-AA <syankuan@gmail.com>
2024-08-08 16:19:10 +0800

    problems

--- src/pages/problems/Problems.js
+++ src/pages/problems/Problems.js
@@ -5,6 +5,7 @@ function Problems() {
     const [problems, setProblems] = useState([]);
     const [page, setPage] = useState(1);
     const [problemsPerPage, setProblemsPerPage] = useState(10);
+    const [problemsCount, setProblemsCount] = useState(0);
     return (
     <div className="problems">
         <table>
@@ -33,7 +34,7 @@ function Problems() {
         <div>
             <label>Page:</label>
             <input type="number" value={page} onChange={(e) => setPage(e.target.value)} />
-            <span></span>
+            <span>/{Math.ceil(problemsCount / problemsPerPage)}</span>
         </div>
         <button onClick={() => setPage(page + 1)}>Next</button>
     </div>
